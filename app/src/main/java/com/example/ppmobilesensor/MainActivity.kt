44103981508be4e0b44b65dbb492c2ee68e2c591
Hieu Nguyen <nth937@gmail.com>
2022-10-23 17:38:25 +0300

    set up Polar sdk, api

--- app/src/main/java/com/example/ppmobilesensor/MainActivity.kt
+++ app/src/main/java/com/example/ppmobilesensor/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.ppmobilesensor
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object {  // ~ static JAVA
+        private const val TAG = "MainActivity"
+        private const val API_LOGGER_TAG = "API LOGGER"
+        private const val PERMISSION_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT), PERMISSION_REQUEST_CODE)
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+            }
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
     }
 }
