bdc4f8653a533cb67fcca788d4c15908482b276d
Hieu Nguyen <nth937@gmail.com>
2022-10-23 18:52:06 +0300

    connect to sensor via bluetooth

--- app/src/main/java/com/example/ppmobilesensor/MainActivity.kt
+++ app/src/main/java/com/example/ppmobilesensor/MainActivity.kt
@@ -4,6 +4,20 @@ import android.Manifest
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.core.graphics.drawable.DrawableCompat
+import com.androidplot.xy.XYPlot
+import com.polar.sdk.api.PolarBleApi
+import com.polar.sdk.api.PolarBleApiCallback
+import com.polar.sdk.api.PolarBleApiDefaultImpl
+import com.polar.sdk.api.errors.PolarInvalidArgument
+import com.polar.sdk.api.model.PolarDeviceInfo
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {  // ~ static JAVA
@@ -12,9 +26,98 @@ class MainActivity : AppCompatActivity() {
         private const val PERMISSION_REQUEST_CODE = 1
     }
 
+    // ATTENTION! Replace with the device ID from your device.
+    private var deviceId = "B5E5C221"
+
+    private val api: PolarBleApi by lazy {
+        // Notice PolarBleApi.ALL_FEATURES are enabled
+        PolarBleApiDefaultImpl.defaultImplementation(applicationContext, PolarBleApi.ALL_FEATURES)
+    }
+
+    private var deviceConnected = false
+    private var bluetoothEnabled = false
+
+    private lateinit var connectButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "version: " + PolarBleApiDefaultImpl.versionInfo())
+        connectButton = findViewById(R.id.connect_button)
+
+        api.setPolarFilter(false)
+        api.setApiLogger { s: String -> Log.d(API_LOGGER_TAG, s) }
+        api.setApiCallback(object : PolarBleApiCallback() {
+            override fun blePowerStateChanged(powered: Boolean) {
+                Log.d(TAG, "BLE power: $powered")
+                bluetoothEnabled = powered
+                if (powered) {
+                    enableAllButtons()
+                    showToast("Phone Bluetooth on")
+                } else {
+                    disableAllButtons()
+                    showToast("Phone Bluetooth off")
+                }
+            }
+
+            override fun deviceConnected(polarDeviceInfo: PolarDeviceInfo) {
+                Log.d(TAG, "CONNECTED: " + polarDeviceInfo.deviceId)
+                deviceId = polarDeviceInfo.deviceId
+                deviceConnected = true
+                val buttonText = getString(R.string.disconnect_from_device, deviceId)
+                toggleButtonDown(connectButton, buttonText)
+            }
+
+            override fun deviceConnecting(polarDeviceInfo: PolarDeviceInfo) {
+                Log.d(TAG, "CONNECTING: " + polarDeviceInfo.deviceId)
+            }
+
+            override fun deviceDisconnected(polarDeviceInfo: PolarDeviceInfo) {
+                Log.d(TAG, "DISCONNECTED: " + polarDeviceInfo.deviceId)
+                deviceConnected = false
+                val buttonText = getString(R.string.connect_to_device, deviceId)
+                toggleButtonUp(connectButton, buttonText)
+            }
+
+            override fun streamingFeaturesReady(
+                identifier: String, features: Set<PolarBleApi.DeviceStreamingFeature>
+            ) {
+                for (feature in features) {
+                    Log.d(TAG, "Streaming feature $feature is ready")
+                }
+            }
+
+            override fun disInformationReceived(identifier: String, uuid: UUID, value: String) {
+                Log.d(TAG, "uuid: $uuid value: $value")
+            }
+
+            override fun batteryLevelReceived(identifier: String, level: Int) {
+                Log.d(TAG, "BATTERY LEVEL: $level")
+            }
+
+            override fun polarFtpFeatureReady(s: String) {
+                Log.d(TAG, "FTP ready")
+            }
+        })
+
+        connectButton.text = getString(R.string.connect_to_device, deviceId)
+        connectButton.setOnClickListener {
+            try {
+                if (deviceConnected) {
+                    api.disconnectFromDevice(deviceId)
+                } else {
+                    api.connectToDevice(deviceId)
+                }
+            } catch (polarInvalidArgument: PolarInvalidArgument) {
+                val attempt = if (deviceConnected) {
+                    "disconnect"
+                } else {
+                    "connect"
+                }
+                Log.e(TAG, "Failed to $attempt. Reason $polarInvalidArgument ")
+            }
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -26,4 +129,65 @@ class MainActivity : AppCompatActivity() {
             requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
         }
     }
+
+    public override fun onPause() {
+        super.onPause()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        api.foregroundEntered()
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        api.shutDown()
+    }
+
+    private fun toggleButtonDown(button: Button, text: String? = null) {
+        toggleButton(button, true, text)
+    }
+
+    private fun toggleButtonDown(button: Button, @StringRes resourceId: Int) {
+        toggleButton(button, true, getString(resourceId))
+    }
+
+    private fun toggleButtonUp(button: Button, text: String? = null) {
+        toggleButton(button, false, text)
+    }
+
+    private fun toggleButtonUp(button: Button, @StringRes resourceId: Int) {
+        toggleButton(button, false, getString(resourceId))
+    }
+
+    private fun toggleButton(button: Button, isDown: Boolean, text: String? = null) {
+        if (text != null) button.text = text
+
+        var buttonDrawable = button.background
+        buttonDrawable = DrawableCompat.wrap(buttonDrawable!!)
+        if (isDown) {
+            DrawableCompat.setTint(buttonDrawable, resources.getColor(R.color.primaryDarkColor))
+        } else {
+            DrawableCompat.setTint(buttonDrawable, resources.getColor(R.color.primaryColor))
+        }
+        button.background = buttonDrawable
+    }
+
+    private fun showToast(message: String) {
+        val toast = Toast.makeText(applicationContext, message, Toast.LENGTH_LONG)
+        toast.show()
+
+    }
+
+    private fun disableAllButtons() {
+        connectButton.isEnabled = false
+    }
+
+    private fun enableAllButtons() {
+        connectButton.isEnabled = true
+    }
+
+//    private fun disposeAllStreams() {
+//        accDisposable?.dispose()
+//    }
 }
